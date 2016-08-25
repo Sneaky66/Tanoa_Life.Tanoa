d488e81c2c561fbd67c5029add96fbecf33d8dd5
AlaskaVet <Tomjennings1994@hotmail.com>
2016-08-25 02:03:11 -0800

    Update common.hpp
    Updated

--- dialog/common.hpp
+++ dialog/common.hpp
@@ -60,6 +60,106 @@ class Life_Checkbox
 
 };
 
+class RscButton
+{
+	access = 0;
+	type = 1;
+	text = "";
+	colorText[] = 
+	{
+		0, // 1
+		0, // 1
+		0, // 1
+		0 // 1
+	};
+	colorDisabled[] = 
+	{
+		0, // 0.4
+		0, // 0.4
+		0, // 0.4
+		1 // 1
+	};
+	colorBackground[] = 
+	{
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		0 // 0.7
+	};
+	colorBackgroundDisabled[] = 
+	{
+		0, // 0.95,
+		0, // 0.95,
+		0, // 0.95,
+		0 // 1
+	};
+	colorBackgroundActive[] = 
+	{
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		0 // 1
+	};
+	colorFocused[] = 
+	{
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_R',0.69])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_G',0.75])",
+		0, // "(profilenamespace getvariable ['GUI_BCG_RGB_B',0.5])",
+		0 // 1
+	};
+	colorShadow[] = 
+	{
+		0,
+		0,
+		0,
+		0 // 1
+	};
+	colorBorder[] = 
+	{
+		0,
+		0,
+		0,
+		1
+	};
+	soundEnter[] = 
+	{
+		"\A3\ui_f\data\sound\RscButton\soundEnter",
+		0.09,
+		1
+	};
+	soundPush[] = 
+	{
+		"\A3\ui_f\data\sound\RscButton\soundPush",
+		0.09,
+		1
+	};
+	soundClick[] = 
+	{
+		"\A3\ui_f\data\sound\RscButton\soundClick",
+		0.09,
+		1
+	};
+	soundEscape[] = 
+	{
+		"\A3\ui_f\data\sound\RscButton\soundEscape",
+		0.09,
+		1
+	};
+	style = 2;
+	x = 0;
+	y = 0;
+	w = 0.095589;
+	h = 0.039216;
+	shadow = 2;
+	font = "EtelkaNarrowMediumPro";
+	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+	offsetX = 0.003;
+	offsetY = 0.003;
+	offsetPressedX = 0.002;
+	offsetPressedY = 0.002;
+	borderSize = 0;
+};
+
 class RscPicture
 {
 	deletable = 0;
